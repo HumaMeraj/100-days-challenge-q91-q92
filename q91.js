011e36b6240a6205c9b2e8a8462612001ce70aa7
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-05-07 01:21:47 +0500

    File of q91

---
+++ q91.js
@@ -0,0 +1,5 @@
+//Create an array of your three favorite fruits and add a new fruit to the end of the array.
+// Defines an array with three favorite fruits
+var favoriteFruits = ["Apple", "Mango", "Cherry"];
+favoriteFruits.push("Banana"); // Adds "Mango" to the end of the array
+console.log(favoriteFruits); // Outputs: ['Apple', 'Banana', 'Cherry', 'Mango']
